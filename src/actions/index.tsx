4e92095872188058adc5c8fbd59626d35c8cf348
Xavier Murcia <xavier.murcia@wefoxgroup.com>
2019-09-21 19:37:28 +0200

    Example on how to handle async operations in react redux

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,7 +1,19 @@
+import axios from 'axios';
+
 export const selectUser = (user_info: any) => {
   return {
     type: 'SELECT_USER',
     payload: user_info
   };
 };
+
+export const fetchPosts = () => async (dispatch: any) => {
+  const posts = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+  dispatch({type: 'FETCH_POSTS', payload: posts.data});
+};
+
+export const addPost = (post_info: any) => {
+  return { type: 'ADD_POST', payload: post_info };
+}
 
