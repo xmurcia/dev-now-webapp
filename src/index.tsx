b2deb564db094eec95bb2aab0246b084b263a783
Xavier Murcia <xavier.murcia@wefoxgroup.com>
2019-09-21 19:35:11 +0200

    Use thunk MW for async actions

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import { rootReducer } from './reducers';
 
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.getElementById('root'));
